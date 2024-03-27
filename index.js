fdc898619fe31828fff3ccddde278b8f11bb97bc
Natorise <natoriseishot@gmail.com>
2024-03-27 02:11:31 -0400

    v2

--- index.js
+++ index.js
@@ -1,13 +1,100 @@
+// load pokemon data
 const pokemonDataPromise = fetch("./pokemon.json")
-.then((response) => response.json())
+.then((response) => response.json());
 
+let columns = 7;
+let rows = 4;
+let totalItems = columns*rows;
 
-// remove accents etc
-function normalize(text) {
-    return text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+const spawnsElement = document.getElementById("spawns");
+
+const ppmElement = document.getElementById("ppm");
+
+// reset when tab is pressed
+document.addEventListener("keydown",(e)=>{
+    if(e.key == "Tab") {
+        e.preventDefault();
+        reload()
+    }
+})
+
+const columnSelector = document.getElementById("columnSelector");
+const rowSelector = document.getElementById("rowSelector");
+
+function generateOptions(selector) {
+    for(let i = 1; i <= 15; i++) {
+        const optionElement = document.createElement("option");
+        optionElement.setAttribute("value",i);
+        optionElement.innerText = i;
+        selector.append(optionElement)
+    }
 }
 
-// generate random pokemon based on spawnrates
+generateOptions(columnSelector)
+generateOptions(rowSelector)
+
+columnSelector.value = columns;
+rowSelector.value = rows;
+
+columnSelector.addEventListener("change",e=>onChange(e,"columns"))
+rowSelector.addEventListener("change",e=>onChange(e,"rows"))
+
+function onChange(e,variableName) {
+    const newValue = parseInt(e.target.value)
+
+    if(variableName === "columns") columns = newValue
+    else if(variableName === "rows") rows = newValue
+
+    reload()
+}
+
+let spawns = [];
+let interval;
+let startTime;
+let answered = 0;
+
+function onKeydown(e) {
+    const pokemonInput = e.target
+    const currentSpawnId = parseInt(pokemonInput.parentNode.parentNode.id);
+
+    if(e.key ==="Enter") {
+        const answer = pokemonInput.value.trim();
+        pokemonInput.value = "";
+
+        const spawn = spawns[currentSpawnId]
+        
+        if(spawn.pokemon.names.includes(answer)) {
+            answered++
+            updatePPM(answered,startTime);
+            if(answered >= spawnsElement.children.length) clearInterval(interval)
+        
+            const targetImageElement = e.target.parentNode.getElementsByTagName("img")[0]
+            targetImageElement.classList.add("answeredspawn")
+
+            pokemonInput.disabled = true
+
+            let newSpawnId = currentSpawnId+1;
+            if(newSpawnId === spawnsElement.children.length) newSpawnId = 0;
+            focusSpawn(newSpawnId);
+        }  
+    }else {
+        if(!interval) {
+            startTime = Date.now();
+            interval = setInterval(()=>{
+                updatePPM(answered,startTime);
+            },1000);
+        }
+        
+    }
+    
+}
+
+
+function focusSpawn(i) {
+    let input = spawnsElement.children[i].getElementsByClassName("pokemonInput")[0];
+    input.focus();
+}
+
 async function generateRandomPokemon(count) {
     const pokemonData = await pokemonDataPromise;
     const totalAbundance = pokemonData.reduce((p,c)=>p + c.abundance,0)
@@ -32,22 +119,59 @@ async function generateRandomPokemon(count) {
     return pokemonArr
 }
 
+function generatePokemonDiv(i,pokemon) {
+    // spawn div
+    const spawnElement = document.createElement("div");
+    spawnElement.setAttribute("class","spawn");
+    spawnElement.setAttribute("id",i);
 
-// set words to pokemon names with formatting
-function setWords(wordsElement,pokemonArr,i = 0) {
-    const pokemonNamesArr = pokemonArr.map(x=>x.names[0])
+    // main div which holds all spawn elements; will be centered inside of spawn div
+    const mainElement = document.createElement("div");
+    mainElement.setAttribute("class","main");
 
-    let newStrArr = []
-    if(i > 0) newStrArr.push(`<span id="completed">${pokemonNamesArr.slice(0,i).join(" ")}</span>`)
-    if(i < pokemonNamesArr.length) newStrArr.push(`<u>${pokemonNamesArr[i]}</u>`)
 
-    newStrArr.push(pokemonNamesArr.slice(i+1).join(" "))
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("src",pokemon.image);
+    imgElement.setAttribute("title",`${pokemon.names[0]}`)
+    
+    const brElement = document.createElement("br");
+    
+    const inputElement = document.createElement("input");
+    inputElement.setAttribute("class","pokemonInput")
+    inputElement.addEventListener("keydown",onKeydown)
+    inputElement.placeholder  = pokemon.names[0]
 
-    wordsElement.innerHTML = newStrArr.join(" ")
+    mainElement.append(imgElement,brElement,inputElement);
+    spawnElement.append(mainElement);
+
+    return spawnElement
+
 }
 
-// update Pokemon per minute
-function updatePPM(ppmElement,i,startTime) {
+async function generateSpawns() {
+    const randomPokemon = await generateRandomPokemon(totalItems)
+
+    console.log(randomPokemon)
+    const elements = []
+
+    for(let i =0; i < randomPokemon.length; i++) {
+        let pokemon = randomPokemon[i]
+        let element = generatePokemonDiv(i,randomPokemon[i]);
+        elements.push(element);
+        spawns[i] = {
+            pokemon:pokemon,
+            element:element,
+            answered:false
+        }
+    }
+    
+    spawnsElement.replaceChildren(...elements)
+    focusSpawn(0)
+}
+
+
+
+function updatePPM(i,startTime) {
     const currentTime = Date.now()
     const timeSince = currentTime-startTime
     let ppm = (i/(timeSince/1000/60))
@@ -57,91 +181,27 @@ function updatePPM(ppmElement,i,startTime) {
     ppmElement.textContent = `${ppm.toFixed(2)} Pokémon per Minute | ${incenses.toFixed(2)} incenses`
 }
 
+function reload() {
+    totalItems = columns*rows
 
+    // asign column & row counts
+    spawnsElement.style.gridTemplateColumns = `repeat(${columns},1fr)` 
+    spawnsElement.style.gridTemplateRows = `repeat(${rows},1fr)` 
 
-window.onload = async ()=>{
-    // get elements
-    const currentWordElement = document.getElementById("currentWord");
-    const wordsElement = document.getElementById("words");
-    const ppmElement = document.getElementById("ppm");
-
-
-    // set defaults
-    currentWordElement.placeholder = "Type to begin";
-    currentWordElement.value = "";
-    currentWordElement.focus();
-    updatePPM(ppmElement,0);
-
-    // generate 25 random pokemon
-    let pokemonArr = await generateRandomPokemon(20);
-    setWords(wordsElement,pokemonArr)
-
-    // track changes to input
-    let i = 0;
-    let currentPokemon = pokemonArr[i];
-    let startTime;
-    let interval;
-
-    let finished = false; 
-    
-
-
-    function onWordCompletion() { 
-        let word = normalize(currentWordElement.value);
-
-        word = word.trimEnd();
-
-        if(currentPokemon.names.includes(word)) {
-            i++;
-            currentWordElement.value = "";
-            setWords(wordsElement,pokemonArr,i)
-            currentPokemon = pokemonArr[i];
-
-            // detect if finished
-            if(i >= pokemonArr.length) {
-                clearInterval(interval);
-                updatePPM(ppmElement,i,startTime);
-                currentWordElement.placeholder = "Press Enter";
-                finished = true;
-            }
-        }
-
-    }
-
-
-    currentWordElement.oninput = (input)=>{
-        if(finished) return;
-        let newLetter = input.data
-        if(!startTime) {
-            currentWordElement.placeholder = ""
-            startTime = Date.now();
-            interval = setInterval(()=>{
-                updatePPM(ppmElement,i,startTime);
-            },1000);
-        }
-
-        let onLastWord = i+1 === pokemonArr.length
-        if(onLastWord ||  newLetter == " ") {
-            onWordCompletion()
-        }
-    }
-    // reset when press enter
-    function onKeydown(e) {
-        if(e.key == "Enter") {
-            if(finished) {
-                currentWordElement.oninput = undefined
-                currentWordElement.removeEventListener("keydown",onKeydown)
-                window.onload()
-            }else {
-                onWordCompletion()
-                currentWordElement.value = "";
-            }
-        }
-    }
-    currentWordElement.addEventListener("keydown",onKeydown)
-
-
-
-
+    clearInterval(interval)
+    interval = undefined;
+    startTime = undefined;
+    spawns = [];
+    answered = 0;
+    generateSpawns()
+    updatePPM(0)
 }
+
+
+reload()
+
+
+
+
+
 
